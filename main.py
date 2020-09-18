43bb029ba7e0fa7bbf1cd332158eec7c0c8065a3
Folafunmi <folafunmimustapha@gmail.com>
2020-09-18 15:10:16 +0100

    Added grid making function

--- main.py
+++ main.py
@@ -79,7 +79,23 @@ class Spot:
         return False
 
 
+# Defining the heuristic function(l-distance)
 def h(p1, p2):
     x1, y1 = p1
     x2, y2 = p2
     return abs(x1 - x2) + abs(y1 - y2)
+
+
+def make_grid(rows, width):
+    grid = []
+    gap = width // rows
+    for i in range(rows):
+        grid.append([])
+        for j in range(rows):
+            spot = Spot(i, j, gap, rows)
+            grid[i].append(spot)
+    return grid
+
+
+def draw_grid(win, row, width):
+    
